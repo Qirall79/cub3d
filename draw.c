ca4c849b984d35ba9e3e1b284197e7c140c2d429
qirall79 <belfatmi79@gmail.com>
2024-04-24 13:10:25 +0100

    Make player block smaller

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: wbelfatm <wbelfatm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 13:14:55 by wbelfatm          #+#    #+#             */
-/*   Updated: 2024/04/24 12:05:27 by wbelfatm         ###   ########.fr       */
+/*   Updated: 2024/04/24 13:00:18 by wbelfatm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,8 @@ void draw_map(t_config *config)
 				y = j * div;
 				while (y < (j + 1) * div)
 				{
-					if (in_range(x, config->xPos * div + config->xOffset, div * (config->xPos + 1) + config->xOffset - 1)
-					&& in_range(y, config->yPos * div + config->yOffset, div * (config->yPos + 1) + config->yOffset - 1))
+					if (in_range(x, config->xPos * div + config->xOffset + 5, div * (config->xPos + 1) + config->xOffset - 5)
+					&& in_range(y, config->yPos * div + config->yOffset + 5, div * (config->yPos + 1) + config->yOffset - 5))
 						mlx_put_pixel(config->img, y, x, 0xFFFF00FF);
 					else if (config->map[i][j] && config->map[i][j] != 5)
 						mlx_put_pixel(config->img, y, x, 0x0FFFFFFF);
