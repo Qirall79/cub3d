95efb07b305112d62a90350d886a36a89f7d4310
qirall79 <belfatmi79@gmail.com>
2024-05-22 19:38:59 +0100

    Fix norm for rendering.c and sprite.c

--- bonus/rendering.c
+++ bonus/rendering.c
@@ -6,7 +6,7 @@
 /*   By: wbelfatm <wbelfatm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 10:39:50 by wbelfatm          #+#    #+#             */
-/*   Updated: 2024/05/21 12:46:28 by wbelfatm         ###   ########.fr       */
+/*   Updated: 2024/05/22 19:28:16 by wbelfatm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ float	get_wall_height(t_config *config, float distance, float alpha, int x)
 	float	correct_dist;
 	float	wall_height;
 
-	plane_dist = config->width / (2.0 * tan((config->fovAngle / 2) * DEG_TO_RAD));
+	plane_dist = config->width
+		/ (2.0 * tan((config->fovAngle / 2) * DEG_TO_RAD));
 	correct_dist = distance * cos((config->viewAngle - alpha) * DEG_TO_RAD);
 	wall_height = roundf(fabs(UNIT / correct_dist) * plane_dist);
 	config->rays[x] = distance;
@@ -43,23 +44,47 @@ void	draw_ceiling_floor(t_config *config, int start_y, int end_y, int x)
 	}
 }
 
-int	get_color(t_config *config, int side, float alpha, t_vector texture_pos)
+t_vector	get_map_pos(t_config *config, t_vector p, float alpha)
 {
-	int	color;
+	t_vector	map_pos;
+
+	map_pos.x = floorf((p.x) / UNIT);
+	map_pos.y = floorf((p.y) / UNIT);
+	if (p.z && horizontal_facing(alpha) == LEFT)
+		map_pos.x--;
+	if (!p.z && vertical_facing(alpha) == TOP)
+		map_pos.y--;
+	return (map_pos);
+}
+
+int	get_color(t_config *config, t_vector p, float alpha, t_vector texture_pos)
+{
+	int			color;
+	t_vector	map_pos;
 
 	color = 0;
-	if (!in_range(texture_pos.y, 0, UNIT - 1) || !in_range(texture_pos.x, 0, UNIT - 1))
-		return 0;
-
-	if (side && horizontal_facing(alpha) == LEFT)
+	map_pos = get_map_pos(config, p, alpha);
+	if (!in_range(texture_pos.y, 0, UNIT - 1)
+		|| !in_range(texture_pos.x, 0, UNIT - 1))
+		return (0);
+	if (config->map[(int)map_pos.y][(int)map_pos.x] == 4)
+		color = config->door_texture[(int)texture_pos.y][(int)texture_pos.x];
+	else if (p.z && horizontal_facing(alpha) == LEFT)
 		color = config->texture_east[(int)texture_pos.y][(int)texture_pos.x];
-	else if (side && horizontal_facing(alpha) == RIGHT)
+	else if (p.z && horizontal_facing(alpha) == RIGHT)
 		color = config->texture_west[(int)texture_pos.y][(int)texture_pos.x];
-	else if (!side && vertical_facing(alpha) == TOP)
+	else if (!p.z && vertical_facing(alpha) == TOP)
 		color = config->texture_north[(int)texture_pos.y][(int)texture_pos.x];
-	else if (!side && vertical_facing(alpha) == DOWN)
+	else if (!p.z && vertical_facing(alpha) == DOWN)
 		color = config->texture_south[(int)texture_pos.y][(int)texture_pos.x];
 	return (color);
+}
+
+int	get_texture_x(t_vector p)
+{
+	if (p.z)
+		return ((int)p.y % UNIT);
+	return ((int)p.x % UNIT);
 }
 
 void	draw_wall(t_config *config, t_vector p, float alpha, int x)
@@ -69,36 +94,20 @@ void	draw_wall(t_config *config, t_vector p, float alpha, int x)
 	float		end_y;
 	float		y;
 	t_vector	texture_pos;
-	t_vector	map_pos;
 
-	map_pos.x = floorf((p.x) / UNIT);
-	map_pos.y = floorf((p.y) / UNIT);
-	if (p.z && horizontal_facing(alpha) == LEFT)
-		map_pos.x--;
-	if (!p.z && vertical_facing(alpha) == TOP)
-		map_pos.y--;
 	wall_height = get_wall_height(config, p.distance, alpha, x);
 	start_y = (config->height / 2) - (wall_height / 2);
 	end_y = start_y + wall_height;
-	if (p.z)
-		texture_pos.x = ((int)p.y % UNIT);
-	else
-		texture_pos.x = ((int)p.x % UNIT);
 	draw_ceiling_floor(config, start_y, end_y, x);
+	texture_pos.x = get_texture_x(p);
 	y = start_y;
 	while (y < end_y && y < config->height)
 	{
-		texture_pos.y = (int)((y - start_y)
-				* ((float) UNIT / wall_height));
+		texture_pos.y = (int)((y - start_y) * ((float) UNIT / wall_height));
 		if (y < 0)
 			y = 0;
-		if (in_range(map_pos.y, 0, config->map_height - 1)
-		&& in_range(map_pos.x, 0, config->map_width - 1)
-		&& config->map[(int)map_pos.y][(int)map_pos.x] == 4)
-			mlx_put_pixel(config->img, x, y, config->door_texture[(int)texture_pos.y][(int)texture_pos.x]);
-		else
-			mlx_put_pixel(config->img, x, y,
-				get_color(config, p.z, alpha, texture_pos));
+		mlx_put_pixel(config->img, x, y,
+			get_color(config, p, alpha, texture_pos));
 		y++;
 	}
 }
--- bonus/sprite.c
+++ bonus/sprite.c
@@ -6,7 +6,7 @@
 /*   By: wbelfatm <wbelfatm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 11:01:46 by wbelfatm          #+#    #+#             */
-/*   Updated: 2024/05/22 12:17:11 by wbelfatm         ###   ########.fr       */
+/*   Updated: 2024/05/22 19:37:51 by wbelfatm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ void	plot_stripes(t_config *config, t_sprite *sprite)
 	}
 }
 
-void	get_sprite_boundaries(t_config *config, t_sprite *sprite)
+void	set_sprite_boundaries(t_config *config, t_sprite *sprite)
 {
 	float	screen_angle;
 	float	fov_ratio;
@@ -71,7 +71,6 @@ void	get_sprite_boundaries(t_config *config, t_sprite *sprite)
 	sprite->end_x = sprite->start_x + sprite->height;
 }
 
-
 void	draw_sprite(t_config *config, t_sprite *sprite)
 {
 	t_vector	diff;
@@ -82,73 +81,63 @@ void	draw_sprite(t_config *config, t_sprite *sprite)
 	diff.y = (sprite->y - config->yPos);
 	sprite->angle = normalize_angle(atan2(diff.y, diff.x) * (1.0 / DEG_TO_RAD));
 	sprite->angle_diff = (config->viewAngle - sprite->angle);
-
-	// if (config->sprite_offset >= 32)
-	// 	config->flying_up = 1;
-	// else if (config->sprite_offset <= 0)
-	// 	config->flying_up = 0;
-
-	// if (config->flying_up)
-	// 	config->sprite_offset--;
-	// else
-	// 	config->sprite_offset++;
-
 	if (in_range(config->viewAngle, 0, 90)
 		&& in_range(sprite->angle, 270, 360))
 		sprite->angle_diff += 360;
 	if (in_range(sprite->angle, 0, 90)
 		&& in_range(config->viewAngle, 270, 360))
 		sprite->angle_diff -= 360;
-	get_sprite_boundaries(config, sprite);
+	set_sprite_boundaries(config, sprite);
 	if (sprite->angle_diff <= (config->fovAngle / 2) + 20)
 		plot_stripes(config, sprite);
 }
 
-void move_sprite(t_config *config, t_sprite *sprite)
+void	set_new_pos(t_config *config, t_vector next_step,
+t_vector map_pos, t_sprite *sprite)
 {
-	t_vector map_pos;
-	t_vector player_pos;
-	t_vector next_step;
-	float distance_to_player;
-	float mov_speed = config->mlx->delta_time * UNIT * 2.0;
+	float	mov_speed;
+
+	mov_speed = config->mlx->delta_time * UNIT * 2.0;
+	if (next_step.x > map_pos.x
+		|| sprite->x - next_step.x * UNIT < UNIT / 2)
+		sprite->x += mov_speed;
+	if ((next_step.x < map_pos.x
+			|| (next_step.x * UNIT - sprite->x) < (UNIT / 2))
+		&& !(sprite->x - next_step.x * UNIT < UNIT / 2))
+		sprite->x -= mov_speed;
+	if (next_step.y > map_pos.y || (sprite->y - next_step.y * UNIT) < UNIT / 2)
+		sprite->y += mov_speed;
+	if ((next_step.y < map_pos.y
+			|| (next_step.y * UNIT - sprite->y) < (UNIT / 2))
+		&& !((sprite->y - next_step.y * UNIT) < UNIT / 2))
+		sprite->y -= mov_speed;
+}
+
+void	move_sprite(t_config *config, t_sprite *sprite)
+{
+	t_vector	map_pos;
+	t_vector	player_pos;
+	t_vector	next_step;
+	float		distance_to_player;
 
 	map_pos.x = floorf(sprite->x / UNIT);
 	map_pos.y = floorf(sprite->y / UNIT);
 	player_pos.x = floorf(config->xPos / UNIT);
 	player_pos.y = floorf(config->yPos / UNIT);
-	
 	if (sprite->path_index >= sprite->path_steps)
 		return ;
-
 	next_step = sprite->path_to_player[sprite->path_index];
-
-	// check if it's in the middle of the next cell
 	if (map_pos.x == floorf(next_step.x)
-	&& map_pos.y == floorf(next_step.y))
+		&& map_pos.y == floorf(next_step.y))
 	{
 		sprite->path_index++;
 		if (sprite->path_index >= sprite->path_steps)
 			return ;
 		next_step = sprite->path_to_player[sprite->path_index];
 	}
-
-	// move sprite
-	if (next_step.x > map_pos.x || sprite->x - next_step.x * UNIT < UNIT / 2)
-		sprite->x += mov_speed;
-	if ((next_step.x < map_pos.x
-	|| (next_step.x * UNIT - sprite->x) < (UNIT / 2)) 
-	&& !(sprite->x - next_step.x * UNIT < UNIT / 2))
-		sprite->x -= mov_speed;
-	if (next_step.y > map_pos.y || (sprite->y - next_step.y * UNIT) < UNIT / 2)
-		sprite->y += mov_speed;
-	if ((next_step.y < map_pos.y
-	|| (next_step.y * UNIT - sprite->y) < (UNIT / 2))
-	&& !((sprite->y - next_step.y * UNIT) < UNIT / 2))
-		sprite->y -= mov_speed;
-	
-	// check if player reached
-	distance_to_player = get_distance(config->xPos, config->yPos, sprite->x, sprite->y);
-
+	set_new_pos(config, next_step, map_pos, sprite);
+	distance_to_player = get_distance(config->xPos, config->yPos,
+			sprite->x, sprite->y);
 	if (distance_to_player < UNIT)
 		config->lost = 1;
 }
